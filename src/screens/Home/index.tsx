e026bc5a50f8d473c2fcf8f1a698e02c3f51a8aa
Gabriel-Rabeloo <gabriel872003@gmail.com>
2021-10-21 17:04:01 -0300

    Configurando eslint, prettier e editorconfig

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native'
+import { View, Text } from 'react-native';
 
 export function Home() {
   return (
     <View>
-      <Text>
-        Olá React Native
-      </Text>
+      <Text>Olá React Native</Text>
     </View>
-  )
+  );
 }
